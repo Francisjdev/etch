ae345b8dc669d04eb3383f8ef1dcf0776a846dea
Francis <franciscorojasdev@proton.me>
2025-03-19 00:17:17 -0300

    most logic part done, need to do the extra part to change the opacity

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 const container = document.querySelector('.container')
+const gridContainer = document.querySelector('.grid-container')
+const button = document.querySelector('button')
+createGrid()
+clickHandler(button)
+
+
+function createGrid(grid = 256, boxDimensions = '40px'){ 
+    gridContainer.textContent = ''
+    for(let i = 0; i < grid; i++){
+        console.log(grid)
+        let box = document.createElement('div')
+        let color = getRandomColor()
+        
+        box.classList = 'box'
+        box.style.width = boxDimensions
+        box.style.height = boxDimensions
+        box.style.backgroundColor = color
+        mouseOverHandler(box)
+        gridContainer.appendChild(box)
+
+    }
+}
+
+function getRandomColor(){
+    let red = Math.round(Math.random()*256)
+    let green = Math.round(Math.random()*256)
+    let blue = Math.round(Math.random()*256)
+    return color =`rgb(${red}, ${green}, ${blue})`
+}
+function clickHandler(obj){
+    obj.addEventListener('click', (e)=> {
+        let userInput =  prompt("how many squares per side do you want")
+        let boxes = parseInt(userInput) * parseInt(userInput)
+        let size = 640/parseInt(userInput)
+        size = `${size}px`
+        
+        createGrid(boxes, size)
+    })
+}
+function mouseOverHandler(obj){
+    obj.addEventListener('mouseover', () => {
+        let color = getRandomColor()
+        obj.style.backgroundColor = color
+    })
+}
+
